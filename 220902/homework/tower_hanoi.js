7dc6b58de005797e89f87a6806660526da13c44e
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-09-03 13:12:46 +0900

    220903
    초안

---
+++ 220902/homework/tower_hanoi.js
@@ -0,0 +1,10 @@
+let a;
+
+function hanoi(n) {
+  console.log(`입력한 block의 수 : ${n} `);
+  let result = 1;
+  for (;;) {}
+}
+
+a = prompt(`block의 수를 입력하세요.`);
+console.log(`block의 수 : ${a} = 움직인 횟수 :`);
