0d43ba7dbef75d02f51999470109032df80283af
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-09-14 21:34:34 +0900

    220914
    DOM / 출석표 예제 / todoList 수정 보완

---
+++ 220914/student/index.js
@@ -0,0 +1,66 @@
+const headList = [
+  { type: "number", name: "번호" },
+  { type: "name", name: "이름" },
+  { type: "age", name: "나이" },
+  { type: "area", name: "거주지" },
+  { type: "mbti", name: "MBTI" },
+  { type: "bloodType", name: "혈액형" },
+];
+const studentsList = [];
+
+function creatStudent(name, age, area, mbti, bloodType) {
+  studentsList.push({
+    name,
+    age,
+    area,
+    mbti,
+    bloodType,
+  });
+  // 객체 정의 시 객체 내에 다른 변수만을 넣으면 변수명과 변수의 값을 키와 값에 입력한다.
+  // name 매개변수에 정의된 값을 객체의 name 키에 대한 값으로 정의한다.
+  // 간단 예제
+  let temp = 100;
+  const tempObj = {
+    temp,
+    // temp : temp, << 24번째 줄과 같다.
+  };
+  console.log(tempObj);
+}
+
+creatStudent("김성진", 27, "성남", "INTP", "B");
+creatStudent("염예나", 22, "하남", "ENFP", "B");
+creatStudent("정재훈", 30, "강남", "ENTP", "B");
+creatStudent("김재일", 29, "용인", "ENFP", "AB");
+creatStudent("이가원", 27, "광진", "ISFP", "O");
+
+console.log(studentsList);
+
+const tableHeaderElem = document.getElementById("table-header");
+
+headList.forEach((item) => {
+  tableHeaderElem.innerHTML += "<th>" + item.name + "</th>";
+});
+
+const studentsListElem = document.getElementById("data-list");
+
+studentsList.forEach((item, index) => {
+  let tempStr = "<tr>"; // << 임시로 쓸 string을 초기화 한다.
+  headList.forEach((headItem) => {
+    if (headItem.type === "number") {
+      tempStr += `<th>${index + 1}</th>`;
+    } else tempStr += `<td>${item[headItem.type]}</td>`;
+  });
+  tempStr += "</tr>";
+  studentsListElem.innerHTML += tempStr;
+});
+
+// let count = 0;
+// studentListElem.innerHTML = studentsList.reduce((next, curr) => {
+//   return (
+//     next +
+//     `<tr>${headList.reduce((headNext, headCurr) => {
+//       if (headCurr.type === "number") return headNext + `<th>${++count}</th>`;
+//       else return headNext + `<td>${curr[headCurr.type]}</td>`;
+//     }, "")}</tr>`
+//   );
+// }, "");
---
+++ 220914/todoList/index.js
@@ -0,0 +1,75 @@
+/*const checkList = [
+  { type: "checkbox", name: "체크박스" },
+  { type: "text", name: "할일" },
+  { type: "mod", name: "수정" },
+  { type: "dele", name: "삭제" },
+];
+const todoList = [];
+
+function createList(checkbox, text, mod, dele) {
+  todoList.push({
+    checkbox,
+    text,
+    mod,
+    dele,
+  });
+}
+
+createList("", "", "mod1", "dele1");
+console.log(todoList);
+
+todoList.forEach((item)=>{
+
+})
+
+
+const headList = [
+  { type: "nam", name: "체크박스" },
+  { type: "age", name: "할일 목록" },
+  { type: "area", name: "수정" },
+  { type: "mbti", name: "삭제" },
+];
+const studentsList = [];
+
+function createList(name, age, area, mbti) {
+  studentsList.push({
+    name,
+    age,
+    area,
+    mbti,
+  });
+}
+
+const tableHeaderElem = document.getElementById("id-header");
+
+headList.forEach((item) => {
+  tableHeaderElem.innerHTML += "<div>" + item.name + "</div>";
+});
+*/
+
+let inputText = document.getElementById("text"); // input text 창 벨류 값
+let clickAdd = document.getElementById("btn"); // 버튼 클릭 객체 접근
+let addText = document.getElementById("check-list"); // 해야 할 일 객체 추가
+let count = 0;
+clickAdd.onclick = () => {
+  let temp = `<div id="box${count}"><input  type="checkbox" /><label>${inputText.value}</label>
+  <button id="btn-mod">수정</button>
+  <button id="btn-del">삭제</button></div>`;
+  addText.innerHTML += temp;
+  inputText.value = "";
+  console.log(inputText.value);
+  console.log(addText);
+  console.log(addText.innerHTML);
+  count++;
+  console.log(count);
+
+  let clickRemove = document.getElementById("btn-del");
+  let checkedBox = document.getElementById(`box${count}`);
+
+  clickRemove.onclick = () => {
+    if (count) {
+      addText.firstElementChild.innerHTML = "";
+      addText.innerHTML;
+    }
+  };
+};
