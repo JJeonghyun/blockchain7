1bb5cbc93c640e78fe22770234290fc2ef89d0c7
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-10-11 21:39:21 +0900

    221011
    개인 프로젝트 6일차

--- 220826/homework/index.js
+++ 220826/homework/index.js
@@ -4,6 +4,9 @@ let moreClick = document.getElementsByClassName("image-box");
 let artist = document.getElementById("artist");
 let hiddenTitle = document.getElementsByClassName("hidden-title");
 let titleButton = document.getElementsByClassName("title-button");
+let dropDownButton = document.getElementsByClassName("dropdown-button");
+let hiddenLiArtist = document.getElementsByClassName("hidden-li-artist");
+let hiddenLiSchedule = document.getElementsByClassName("hidden-li-schedule");
 
 let imgArr = new Array();
 imgArr[0] = "./Img/MainImg.jpg";
@@ -52,5 +55,17 @@ moreClickSpan.addEventListener("click", () => {
     if (hiddenTitle[0].style.display == "none")
       hiddenTitle[0].style.display = "block";
     else hiddenTitle[0].style.display = "none";
+  });
+});
+dropDownButton[0].addEventListener("click", () => {
+  [...hiddenLiArtist].forEach((elem) => {
+    if (elem.style.display == "block") elem.style.display = "none";
+    else elem.style.display = "block";
+  });
+});
+dropDownButton[1].addEventListener("click", () => {
+  [...hiddenLiSchedule].forEach((elem) => {
+    if (elem.style.display == "block") elem.style.display = "none";
+    else elem.style.display = "block";
   });
 });
